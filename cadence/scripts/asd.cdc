35b17183e4fc76f1ce2c257c2c70609e161cdeda
tdv <tdv@princeton.edu>
2023-04-20 20:17:48 -0400

    fix: add flow to package.json

--- cadence/scripts/asd.cdc
+++
@@ -1,7 +0,0 @@
-import ExampleNFT from "../contracts/ExampleNFT.cdc"
-
-pub fun main(acct_addr: Address) {
-    log("sdaffdsafasd")
-    log(acct_addr)
-}
-
--- v1/rest-nft-dapp/src/App.js
+++ v1/rest-nft-dapp/src/App.js
@@ -1,20 +1,18 @@
-import './App.css';
-import react, { useState, useEffect } from 'react';
+import "./App.css";
+import react, { useState, useEffect } from "react";
 import * as fcl from "@onflow/fcl";
-import {getData} from './cadence/scripts/getData.js'
-import {setup} from './cadence/transactions/setup.js'
-import {mint} from './cadence/transactions/mint.js'
+import { getData } from "./cadence/scripts/getData.js";
+import { setup } from "./cadence/transactions/setup.js";
+import { mint } from "./cadence/transactions/mint.js";
 
-fcl.config()
+fcl
+  .config()
   .put("app.detail.title", "NFT Dapp")
   .put("app.detail.icon", "https://i.imgur.com/9I6NRUm.png")
   .put("accessNode.api", "https://rest-testnet.onflow.org")
-  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
-
-
+  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn");
 
 function App() {
-
   const [user, setUser] = useState();
   const [totalSupply, setTotalSupply] = useState();
   const [myNFT, setMyNFT] = useState();
@@ -26,63 +24,66 @@ function App() {
   const login = () => {
     fcl.authenticate();
     fcl.currentUser().subscribe(setUser);
-  }
+  };
 
   const logout = () => {
     fcl.unauthenticate();
     setUser(null);
-  }
+  };
 
   const GetMyNFT = async () => {
-    
     const response = await fcl.send([
       fcl.script(getData),
-      fcl.args([
-        fcl.arg(user.addr, fcl.t.Address)
-      ])
-    ])
-    const data = await fcl.decode(response)
+      fcl.args([fcl.arg(user.addr, fcl.t.Address)]),
+    ]);
+    const data = await fcl.decode(response);
     setMyNFT(data);
-    console.log(data)
-    console.log(myNFT)
-    console.log(myNFT.type)
-  }
+    console.log(data);
+    console.log(myNFT);
+    console.log(myNFT.type);
+  };
 
-  const mintNFT = async () =>{
-    const transactionID = await fcl.send([
-      fcl.transaction(mint),
-      fcl.args([fcl.arg(mintData, fcl.t.String), fcl.arg(mintBond, fcl.t.UInt64), fcl.arg(mintRoyalties, fcl.t.UFix64), fcl.arg(mintName, fcl.t.String)]),
-      fcl.payer(fcl.authz),
-      fcl.proposer(fcl.authz),
-      fcl.authorizations([fcl.authz]),
-      fcl.limit(9999)
-    ]).then(fcl.decode)
+  const mintNFT = async () => {
+    const transactionID = await fcl
+      .send([
+        fcl.transaction(mint),
+        fcl.args([
+          fcl.arg(mintData, fcl.t.String),
+          fcl.arg(mintBond, fcl.t.UInt64),
+          fcl.arg(mintRoyalties, fcl.t.UFix64),
+          fcl.arg(mintName, fcl.t.String),
+        ]),
+        fcl.payer(fcl.authz),
+        fcl.proposer(fcl.authz),
+        fcl.authorizations([fcl.authz]),
+        fcl.limit(9999),
+      ])
+      .then(fcl.decode);
 
-    console.log(transactionID)
-  }
+    console.log(transactionID);
+  };
 
   const setupAccount = async () => {
-    const transactionID = await fcl.send([
-      fcl.transaction(setup),
-      fcl.args(),
-      fcl.payer(fcl.authz),
-      fcl.proposer(fcl.authz),
-      fcl.authorizations([fcl.authz]),
-      fcl.limit(9999)
-    ]).then(fcl.decode)
-    console.log(transactionID)
-  }
-
-
+    const transactionID = await fcl
+      .send([
+        fcl.transaction(setup),
+        fcl.args(),
+        fcl.payer(fcl.authz),
+        fcl.proposer(fcl.authz),
+        fcl.authorizations([fcl.authz]),
+        fcl.limit(9999),
+      ])
+      .then(fcl.decode);
+    console.log(transactionID);
+  };
 
   return (
     <div className="App">
       <h1> Flow NFT </h1>
-      <h2> Current Address : {user && user.addr ? user.addr : ''} </h2>
-      <button onClick={login}
-       > Login </button>
+      <h2> Current Address : {user && user.addr ? user.addr : ""} </h2>
+      <button onClick={login}> Login </button>
       <p> ----------------------</p>
-      
+
       <button onClick={() => setupAccount()}> Setup </button>
       <p> ----------------------</p>
       <input
@@ -109,22 +110,23 @@ function App() {
         value={mintRoyalties}
         onChange={(e) => setMintRoyalties(e.target.value)}
       />
-      
+
       <br />
       <button onClick={() => mintNFT()}> Create NFT </button>
 
       <p> ----------------------</p>
       <button onClick={() => GetMyNFT()}> Get NFT </button>
       <p> ----------------------</p>
-      
-      {myNFT ? myNFT.map((nft, index) => (
-        <div key={index}>
-          <img src={nft.Data} />
-        </div>
-      )) : ""}
+
+      {myNFT
+        ? myNFT.map((nft, index) => (
+            <div key={index}>
+              <img src={nft.Data} />
+            </div>
+          ))
+        : ""}
       {myNFT ? myNFT.length : 0}
       <p> ----------------------</p>
-
     </div>
   );
 }
