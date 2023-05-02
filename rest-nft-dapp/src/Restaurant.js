07879e3a8e8f9678c8f4784903ac613205d23907
tdv <tdv@princeton.edu>
2023-05-02 18:24:31 -0400

    ft: add restuarant page

---
+++ rest-nft-dapp/src/Restaurant.js
@@ -0,0 +1,50 @@
+import react, { useState, useEffect } from "react";
+import * as fcl from "@onflow/fcl";
+import { getData } from "./cadence/scripts/getData.js";
+import { useParams } from "react-router-dom";
+
+fcl
+  .config()
+  .put("app.detail.title", "NFT Dapp")
+  .put("app.detail.icon", "https://i.imgur.com/9I6NRUm.png")
+  .put("accessNode.api", "https://rest-testnet.onflow.org")
+  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn");
+
+function Restaurant() {
+  const [myNFT, setMyNFT] = useState();
+  const params = useParams();
+  const publicKey = params.publicKey;
+
+  const GetMyNFT = async () => {
+    const response = await fcl.send([
+      fcl.script(getData),
+      fcl.args([fcl.arg(publicKey, fcl.t.Address)]),
+    ]);
+    const data = await fcl.decode(response);
+    setMyNFT(data);
+    console.log(data);
+    console.log(myNFT);
+    console.log(myNFT.type);
+  };
+
+  
+  return (
+    <div className="App">
+      <p> ----------------------</p>
+      <button onClick={() => GetMyNFT()}> Get NFT </button>
+      <p> ----------------------</p>
+
+      {myNFT
+        ? myNFT.map((nft, index) => (
+            <div key={index}>
+              <img src={nft.Data} />
+            </div>
+          ))
+        : ""}
+      {myNFT ? myNFT.length : 0}
+      <p> ----------------------</p>
+    </div>
+  );
+}
+
+export default Restaurant;
--- rest-nft-dapp/src/index.js
+++ rest-nft-dapp/src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Restaurant from './Restaurant';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "restaurants/:publicKey",
+    element: <Restaurant />,
+  },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
