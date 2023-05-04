1d355aba82f7e329ede6ef98c24f2f9c766b51cc
tdv <tdv@princeton.edu>
2023-05-03 21:54:47 -0400

    fix: fix getting nfts by restaurant id

--- resnft-nextjs/src/layout/Layout.js
+++ resnft-nextjs/src/layout/Layout.js
@@ -2,12 +2,6 @@ import Navbar from "@/components/navbar";
 import { useAuthContext } from "@/contexts/AuthContext.js";
 
 export default function Layout({ children }) {
-  const { user, _, authz, __ } = useAuthContext();
-  console.log("Layout");
-  console.log(user);
-  console.log(authz);
-  console.log("--------")
-
   return (
     <>
       <Navbar />
--- resnft-nextjs/src/pages/restaurants/[publickey].js
+++ resnft-nextjs/src/pages/restaurants/[publickey].js
@@ -1,48 +1,40 @@
 import react, { useState, useEffect } from "react";
 import * as fcl from "@onflow/fcl";
 import { getData } from "../../cadence/scripts/getData.js";
-import { useRouter } from 'next/router'
-
-fcl
-  .config()
-  .put("app.detail.title", "NFT Dapp")
-  .put("app.detail.icon", "https://i.imgur.com/9I6NRUm.png")
-  .put("accessNode.api", "https://rest-testnet.onflow.org")
-  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn");
+import { useRouter } from "next/router";
 
 function Restaurant() {
-  const [myNFT, setMyNFT] = useState();
-  const router = useRouter()
-  const { publickey } = router.query
+  const router = useRouter();
+  const { publickey } = router.query;
+  const addr = publickey;
 
-  const GetMyNFT = async () => {
-    const response = await fcl.send([
-      fcl.script(getData),
-      fcl.args([fcl.arg(publickey, fcl.t.Address)]),
-    ]);
-    const data = await fcl.decode(response);
-    setMyNFT(data);
-    console.log(data);
-    console.log(myNFT);
-    console.log(myNFT.type);
-  };
+  const [nfts, setNfts] = useState(null);
 
-  
+  useEffect(() => {
+    const fetchNFT = async ({ addr }) => {
+      const response = await fcl.send([
+        fcl.script(getData),
+        fcl.args([fcl.arg(addr, fcl.t.Address)]),
+      ]);
+      const data = await fcl.decode(response);
+      setNfts(data);
+    };
+    if (addr) {
+      fetchNFT({ addr });
+    }
+  }, [addr]);
+
   return (
-    <div className="App">
-      <p> ----------------------</p>
-      <button onClick={() => GetMyNFT()}> Get NFT </button>
-      <p> ----------------------</p>
-
-      {myNFT
-        ? myNFT.map((nft, index) => (
+    <div>
+      {nfts
+        ? nfts.map((nft, index) => (
             <div key={index}>
               <img src={nft.Data} />
             </div>
           ))
         : ""}
-      {myNFT ? myNFT.length : 0}
-      <p> ----------------------</p>
+
+      {nfts ? nfts.length : 0}
     </div>
   );
 }
