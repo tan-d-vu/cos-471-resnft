cde03605b3961ab53356ed867db437fa25a567c8
tdv <tdv@princeton.edu>
2023-05-04 21:18:48 -0400

    fix: change create to upsert in update profile

--- resnft-nextjs/src/pages/api/users/update.js
+++ resnft-nextjs/src/pages/api/users/update.js
@@ -12,20 +12,23 @@ export default async function handler(req, res) {
 
   const isRestaurant = userType === "restaurant";
 
-  // Create new user
-  const user = await prisma.user.create({
-    data: {
-      pubKey: pubKey,
-      isRestaurant: isRestaurant,
-      name: name,
-      email: email,
-      phone: phone,
-      location: location,
-      menu: menu,
-      description: description,
-    },
-  });
+  const data = {
+    pubKey: pubKey,
+    isRestaurant: isRestaurant,
+    name: name,
+    email: email,
+    phone: phone,
+    location: location,
+    menu: menu,
+    description: description,
+  };
 
+  // Create new user
+  const user = await prisma.user.upsert({
+    where: { pubKey: pubKey },
+    update: data,
+    create: data,
+  });
 
   return res.status(200).json({ message: "User profile updated successfully" });
 }
