0102040635d25771992a9dfa7ea018e59b576b88
aririggins <ariggins@princeton.edu>
2023-05-09 02:09:42 -0400

    adding footer

---
+++ resnft-nextjs/src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+function Footer() {
+  
+  return (
+    <div className="footer">
+      <div className="foot-container">
+        &copy; This site was created by the ResNFT team
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
