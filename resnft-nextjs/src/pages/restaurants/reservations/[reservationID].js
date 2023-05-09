be15ac9e33e55ce05b89a84d74416bfa81943c6e
tdv <tdv@princeton.edu>
2023-05-08 20:56:45 -0400

    fix: fix purchase

--- resnft-nextjs/src/pages/restaurants/reservations/[reservationID].js
+++ resnft-nextjs/src/pages/restaurants/reservations/[reservationID].js
@@ -19,13 +19,16 @@ const ShowReservationToBook = ({ reservation }) => {
   }, []);
 
   // transaction(addr: Address, tokenID: UInt64, price: UFix64) {
-  const purchase = async ({ addr, tokenID, price }) => {
+  const makePurchase = async ({ addr, tokenID, price }) => {
+    console.log(addr);
+    console.log(tokenID);
+    console.log(price);
     const authz = fcl.currentUser().authorization;
     return await fcl
       .send([
         fcl.transaction(purchase),
         fcl.args([
-          fcl.arg(addr, fcl.t.String),
+          fcl.arg(addr, fcl.t.Address),
           fcl.arg(tokenID, fcl.t.UInt64),
           fcl.arg(price, fcl.t.UFix64),
         ]),
@@ -34,52 +37,60 @@ const ShowReservationToBook = ({ reservation }) => {
         fcl.authorizations([authz]),
         fcl.limit(9999),
       ])
-      .then(fcl.decode);
+      .then((res) => {
+        console.log(fcl.decode(res));
+        return fcl.tx(res).onceSealed();
+      });
   };
 
   const handlePurchase = async () => {
-    const buyerAddr = user.addr;
+    const buyerAddr = reservation.restaurantID;
     const tokenID = reservation.nft;
-    await purchase({buyerAddr, tokenID, salePrice});
+
+    await makePurchase({
+      addr: reservation.restaurantID,
+      tokenID: tokenID,
+      price: salePrice,
+    }).then((res) => {
+      console.log(res);
+    });
   };
 
   console.log(reservation);
   // keys {id, content, restaurantID, datetime, isAvailable, nft})
   return (
     <>
-        {reservation.datetime}
-        <br />
-        {reservation.nft}
-        <br />
-        {reservation.content}
-        <br />
-        {reservation.restaurantID}
-        <br />
-        Price: {salePrice}
-        <br />
-        <>
-          {reservation.isAvailable ? (
-            <>
-              {!user || !user.addr ? (
-                <>Log in to book this reservation</>
-              ) : (
-                <>
-                  {user.addr != reservation.restaurantID ? (
-                    <>
-                    <button onClick={() => handlePurchase()}>
-                      Purchase
-                    </button>
-                    </>
-                  ) : (
-                    <>You own this reservation</>
-                  )}
-                </>
-              )}
-            </>
-          ) : (
-            <>This reservation is not available</>
-          )}
-        </>
+      {reservation.datetime}
+      <br />
+      {reservation.nft}
+      <br />
+      {reservation.content}
+      <br />
+      {reservation.restaurantID}
+      <br />
+      Price: {salePrice}
+      <br />
+      <>
+        {reservation.isAvailable ? (
+          <>
+            {!user || !user.addr ? (
+              <>Log in to book this reservation</>
+            ) : (
+              <>
+                {user.addr != reservation.restaurantID ? (
+                  <>
+                    <button onClick={() => handlePurchase()}>Purchase</button>
+                  </>
+                ) : (
+                  <>You own this reservation</>
+                )}
+              </>
+            )}
+          </>
+        ) : (
+          <>This reservation is not available</>
+        )}
+      </>
     </>
   );
 };
