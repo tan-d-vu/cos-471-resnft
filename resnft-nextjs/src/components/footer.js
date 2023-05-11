5ac88a5800f39921bda6004b15546fbcb15c9ba7
tdv <tdv@princeton.edu>
2023-05-11 18:45:27 -0400

    frontend-redo: footer

--- resnft-nextjs/src/components/footer.js
+++ resnft-nextjs/src/components/footer.js
@@ -2,10 +2,8 @@ import React, { useEffect } from "react";
 
 function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full">
-      <div className="pt-2 pb-3 text-center text-green">
+    <footer className="fixed bottom-0 w-full pt-2 pb-3 text-center text-green">
         &copy; Created by the ResNFT Team
-      </div>
     </footer>
   );
 }
